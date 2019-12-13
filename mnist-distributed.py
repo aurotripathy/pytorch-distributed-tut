93f4caa4896fc3ad1f394cebb71b0c489d145299
Auro Tripathy <ipserv@yahoo.com>
2019-12-13 06:29:03 +0000

    WIP

--- mnist-distributed.py
+++ mnist-distributed.py
@@ -44,9 +44,25 @@ def train(gpu, args):
                                                pin_memory=True,
                                                sampler=train_sampler)
 
+    val_dataset = torchvision.datasets.MNIST(root='./data',
+                                               train=False,
+                                               transform=transforms.ToTensor(),
+                                               download=True)
+    val_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset,
+                                                                    num_replicas=args.world_size,
+                                                                    rank=rank)
+    val_loader = torch.utils.data.DataLoader(dataset=val_dataset,
+                                               batch_size=batch_size,
+                                               shuffle=False,
+                                               num_workers=0,
+                                               pin_memory=True,
+                                               sampler=val_sampler)
+
     start = datetime.now()
-    total_step = len(train_loader)
+    total_train_step = len(train_loader)
+    total_val_step = len(val_loader)
     for epoch in range(args.epochs):
+        model.train()  # train mode
         for i, (images, labels) in enumerate(train_loader):
             images = images.cuda(non_blocking=True)
             labels = labels.cuda(non_blocking=True)
@@ -59,8 +75,25 @@ def train(gpu, args):
             loss.backward()
             optimizer.step()
             if (i + 1) % 10 == 0 and gpu == 0:
-                print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch + 1, args.epochs, i + 1, total_step,
+                print('Epoch [{}/{}], Step [{}/{}], Train Loss: {:.4f}'.format(epoch + 1, args.epochs, i + 1, total_train_step,
                                                                          loss.item()))
+        model.eval() # evaluate mode
+        correct = 0
+        with torch.no_grad():
+            for i, (images, labels) in enumerate(val_loader):
+                images, labels = images.cuda(), labels.cuda()
+                outputs = model(images)
+                val_loss = criterion(outputs, labels)
+                pred = outputs.argmax(dim=1, keepdim=True)  # get the index of the max 
+                correct += pred.eq(labels.view_as(pred)).sum().item()
+                if (i + 1) % 50 == 0 and gpu == 0:
+                    print('Epoch [{}/{}], Step [{}/{}], Val Loss: {:.4f}'.format(epoch + 1, args.epochs, i + 1, total_val_step,
+                                                                             val_loss.item()))
+
+
+        print('\nValidation Accuracy: {}/{} ({:.0f}%)\n'.format(
+            correct, len(val_loader.dataset),
+            100. * correct / len(val_loader.dataset)))
     if gpu == 0:
         print("Training complete in: " + str(datetime.now() - start))
 
